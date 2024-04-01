3e64f3a67752769fa57cf8e0dee19fe4b24a83e5
Kouzou Akizuki <Oriverosp@unal.edu.co>
2024-04-01 12:08:48 -0500

    Graficas

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from multiprocessing import	Process, Manager, shared_memory, Condition
 
 import numpy as np
 
+import pyqtgraph as pg
+from pyqtgraph.Qt import QtGui, QtCore
+
 #### URAD Parameters
 mode 	= 1 	# 1->Doppler, 2->Diente de sierra, 3-> Triangular, 4->Triangular con doble frecuencia
 f0		= 5 	# Frecuencia inicial en MHz, ->25.xxxGHz, [5 - 245]MHz
@@ -26,7 +29,6 @@ def CloseProgram():
     return_code = uRAD_RP_SDK11.turnOFF()
     if(return_code != 0):
         exit()
-
 
 def Urad_Samples(Data_Condition):
 
@@ -55,6 +57,55 @@ def Urad_Samples(Data_Condition):
             Raw_Data_mem.close()
             Data_Condition.notify()
 
+def update_1(graph,data_y,num):
+        
+        Y_mem = shared_memory.SharedMemory(name=data_y)
+
+        Y_data = np.ndarray((num), dtype=np.float64, buffer=Y_mem.buf)
+
+        graph.setData(np.array(range(1,num,1)),np.array(Y_data))
+
+        Y_mem.close()   
+
+def update_2(graph,data_x,data_y,num):
+        
+        X_mem = shared_memory.SharedMemory(name=data_x)
+        Y_mem = shared_memory.SharedMemory(name=data_y)
+
+        X_data = np.ndarray((num), dtype=np.float64, buffer=X_mem.buf)
+        Y_data = np.ndarray((num), dtype=np.float64, buffer=Y_mem.buf)
+
+        graph.setData(np.array(X_data),np.array(Y_data))
+
+        X_mem.close()    
+        Y_mem.close()   
+
+def Graph_Pyqtgraph_Core(Title_1):
+    app = pg.mkQApp(Title_1)
+    win = pg.GraphicsLayoutWidget(show=True, title=Title_1)
+    win.resize(1000,600)
+    win.setWindowTitle(Title_1)
+
+    # Enable antialiasing for prettier plots
+    pg.setConfigOptions(antialias=True)
+
+
+    p1 = win.addPlot()
+    curve_1 = p1.plot(pen='y')
+    p1.setYRange(1, -1, padding=0)
+    p1.enableAutoRange('xy', False)  ## stop auto-scaling after the first data set is plotted
+    
+    
+    timer_1 = QtCore.QTimer()
+    timer_1.timeout.connect(lambda:   update_1(curve_1,'RawData',Ns))
+    
+    timer_1.start()
+
+
+    pg.exec()
+
+
+
 if __name__ == '__main__':
     raw_data_size_IQ        =   np.dtype(np.float64).itemsize*2*Ns
     raw_data_size_normal    =   np.dtype(np.float64).itemsize*Ns
@@ -65,6 +116,8 @@ if __name__ == '__main__':
     Raw_Data        =   np.ndarray(shape=(2,Ns), dtype=np.float64, buffer=Raw_Data_mem.buf)
 
     Read=Process(target=Urad_Samples, args=(Rx_Data_ready,))
+    Graph=Process(target=Graph_Pyqtgraph_Core, args=('Samples'))
+
 
     Read.start()
     Read.join()
